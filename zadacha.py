6383c56b526ee308dbd36ef82fbd2c0732b09751
Rusya575 <78809194+Rusya575@users.noreply.github.com>
2025-05-18 19:09:20 +0300

    Add files via upload

--- zadacha.py
+++ zadacha.py
@@ -1,26 +1,37 @@
-def ans(n, edges):
-    dist = [float('inf')] * (n + 1)
-    dist[0] = 1.0
+def min_edge_cover_bipartite(U_size, V_size, edges):
+    match_U = [-1] * U_size
+    match_V = [-1] * V_size
 
-    for _ in range(20):
-        updated = False
-        for (u, v, rate) in edges:
-            new_val = dist[u] * rate
-            if new_val > dist[v]:
-                dist[v] = new_val
-                updated = True
-        if not updated:
-            break
+    def dfs(u, visited):
+        for v in edges[u]:
+            if not visited[v]:
+                visited[v] = True
+                if match_V[v] == -1 or dfs(match_V[v], visited):
+                    match_U[u] = v
+                    match_V[v] = u
+                    return True
+        return False
 
-    final_rub = dist[0]
-    for (u, v, rate) in edges:
-        if u == 0 and dist[v] * rate > final_rub:
-            return True
-    return False
+    max_matching = 0
+    for u in range(U_size):
+        if dfs(u, [False] * V_size):
+            max_matching += 1
 
-edges = [
-    (0, 1, 2.0),
-    (1, 2, 2.0),
-    (2, 0, 4.0)
-]
-print(ans(2, edges))
+    edge_cover = []
+    for u in range(U_size):
+        if match_U[u] != -1:
+            edge_cover.append((u, match_U[u]))
+
+    for v in range(V_size):
+        if match_V[v] == -1:
+            for u in range(U_size):
+                if v in edges[u]:
+                    edge_cover.append((u, v))
+                    break
+
+    return edge_cover
+
+U_size = 2
+V_size = 3
+edges = [[0, 1], [2]]
+print("Минимальное покрытие:", min_edge_cover_bipartite(U_size, V_size, edges))
